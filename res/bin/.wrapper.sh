ce18f0154fb2f702e379162d1b2e9b7f0053c271
EXP <289065406@qq.com>
2023-09-27 09:04:37 +0800

    更新文档

--- res/bin/.wrapper.sh
+++ res/bin/.wrapper.sh
@@ -8,7 +8,7 @@
 /bin/.docker-entrypoint.sh
 
 
-# 接受外部参数、执行命令 perl openkore.pl --control=xxxxxx
+# 接受外部参数、执行指定控制目录的命令、以便多账号使用 perl openkore.pl --control=xxxxxx
 if [ -n "$1" ]; then
     cd ${OPENKORE_DIR}
     perl ./openkore.pl $1
